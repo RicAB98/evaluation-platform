02853a35a4c027dc01fb3d96e563422a8c7bafc0
RicardoBoia2017 <up201505244@fe.up.pt>
2021-04-02 16:10:12 +0100

    Redirecting to corret initial page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Router history={hist}>
     <Switch>
       <Route path="/admin" component={Admin} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/" to="/admin/neweval" />
     </Switch>
   </Router>,
   document.getElementById("root")
