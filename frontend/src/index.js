ae372eeeb8be11843a900dd9f6eefc10cb8f263c
RicardoBoia2017 <up201505244@fe.up.pt>
2021-04-20 15:42:17 +0100

    FRONTEND - Remove unused components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,6 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
-import favicon from './assets/img/favicon.ico';
 
 const hist = createBrowserHistory();
 
@@ -28,7 +27,4 @@ ReactDOM.render(
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
